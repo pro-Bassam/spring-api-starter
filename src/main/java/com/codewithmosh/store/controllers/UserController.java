963435a5e418c10d06d9a5a3114d00ed005d5a20
Bassam Ehab <bassam.bosax10@gmail.com>
2025-12-20 13:27:14 +0200

    Add x-auth-token header to getAllUsers endpoint
    The getAllUsers method in UserController now accepts an optional 'x-auth-token' request header. This change allows the endpoint to receive and log authentication tokens for potential future use.

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -19,8 +19,11 @@ public class UserController {
 
     @GetMapping
     public Iterable<UserDto> getAllUsers(
+            @RequestHeader(required = false, name = "x-auth-token") String authToken,
             @RequestParam(required = false, defaultValue = "", name = "sort") String sortBy
     ) {
+        System.out.println(authToken);
+
         if (!Set.of("name", "email").contains(sortBy))
             sortBy = "name";
 
